21b03c194294d045a442f41795e9518eb1c47d4b
Braz pereira <103585759+Braz-p@users.noreply.github.com>
2022-05-27 15:00:59 -0300

    Adcionei arquivos via opload

---
+++ java codigo/Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo5/bytebank/src/CriaConta.java
@@ -0,0 +1,20 @@
+public class CriaConta {
+
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.deposita(200);
+        System.out.println(primeiraConta.getSaldo());
+
+        primeiraConta.deposita(100);
+        System.out.println(primeiraConta.getSaldo());
+
+        Conta segundaConta = new Conta();
+        segundaConta.deposita(50);
+
+        System.out.println("primeira conta tem " + primeiraConta.getSaldo());
+        System.out.println("segunda conta tem " + segundaConta.getSaldo());
+
+        System.out.println("referenciar: " + primeiraConta);
+    }
+
+}
